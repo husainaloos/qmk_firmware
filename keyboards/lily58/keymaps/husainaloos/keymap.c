4f6d393232d577a0cdc1379393da0b74ee828d1f
Husain <husain@localhost.localdomain>
2022-01-30 15:15:36 -0600

    add shift and ctrl to the left side

--- keyboards/lily58/keymaps/husainaloos/keymap.c
+++ keyboards/lily58/keymaps/husainaloos/keymap.c
@@ -20,9 +20,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |Backsp|   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------|       |    | ENT   |------+------+------+------+------+------|
- * |      |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |      |
+ * |SHIFT |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |      |
  * `-----------------------------------------/       /     \ Shft \-----------------------------------------'
- *                   | LAlt | GUI  | L1   | /Space  /       \ESCAPE\  | CTRL | ALT  | ENT  |
+ *                   | CTL  | GUI  | L1   | /Space  /       \ESCAPE\  | CTRL | ALT  | ENT  |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -31,8 +31,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   MO(_ADJUST), KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                              KC_6,           KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
   KC_TAB,      KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                              KC_Y,           KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
   KC_BSPC,     KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                              KC_H,           KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  _______,     KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    _______, KC_ENT,          KC_N,           KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______,
-                                KC_LALT, KC_LGUI, TT(_L1), KC_SPC,  RSFT_T(KC_ESC),  RCTL_T(KC_ENT), KC_RALT, KC_ENT
+  KC_LSFT,     KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    _______, KC_ENT,          KC_N,           KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______,
+                                KC_LCTL, KC_LGUI, TT(_L1), KC_SPC,  RSFT_T(KC_ESC),  RCTL_T(KC_ENT), KC_RALT, KC_ENT
 ),
 /* L1
  * ,-----------------------------------------.                    ,-----------------------------------------.
