4a6c137f1dd4264a09ac5b56e9b0a0e8ef94e4b2
husain <husain@localhost.localdomain>
2022-05-15 20:11:33 -0500

    Replace gui and meh key

--- keyboards/lily58/keymaps/husainaloos/keymap.c
+++ keyboards/lily58/keymaps/husainaloos/keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|       |    | ENT   |------+------+------+------+------+------|
  * |ADJUST|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |  \   |
  * `-----------------------------------------/       /     \ Shft \-----------------------------------------'
- *                   | MEH  | ALT  | L1   | /Space  /       \Backsp\  | CTRL | ALT  | GUI  |
+ *                   | GUI  | ALT  | L1   | /Space  /       \Backsp\  | CTRL | ALT  | MEH  |
  *                   |      |      |      |/       /         \      \ | ENTR |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,      KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                              KC_Y,           KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
   _______,     KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                              KC_H,           KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   MO(_ADJUST), KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    _______, KC_ENT,          KC_N,           KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-                                KC_MEH, KC_LALT, MO(_L1), KC_SPC,  RSFT_T(KC_BSPC), RCTL_T(KC_ENT), KC_RALT, KC_RGUI
+                                KC_LGUI, KC_LALT, MO(_L1), KC_SPC,  RSFT_T(KC_BSPC), RCTL_T(KC_ENT), KC_RALT, KC_MEH
 ),
 /* L1
  * ,-----------------------------------------.                    ,-----------------------------------------.
