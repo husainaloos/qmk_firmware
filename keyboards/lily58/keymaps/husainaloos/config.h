fd433deb53033921f86f6397ace5bca7f626c03d
husain <husain@localhost.localdomain>
2022-09-10 10:28:15 -0500

    use permissive hold
    this helps with stuff

--- keyboards/lily58/keymaps/husainaloos/config.h
+++ keyboards/lily58/keymaps/husainaloos/config.h
@@ -27,4 +27,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define EE_HANDS
 
 #define TAPPING_TERM 200
+#define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT // Obey tapping term for mod-tap
