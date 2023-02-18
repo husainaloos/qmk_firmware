5b9f384c6789f07fd4a16ebda4adb1749506359f
Husain Alshehhi <husainaloos@users.nonreply.github.com>
2023-02-18 13:39:58 -0600

    Added layer L2

--- keyboards/lily58/keymaps/husainaloos/keymap.c
+++ keyboards/lily58/keymaps/husainaloos/keymap.c
@@ -5,6 +5,7 @@
 enum layer_number {
 	_QWERTY = 0,
 	_L1,
+	_L2,
 	_NAVIG,
 	_WINCT,
 	_ADJUST,
@@ -20,18 +21,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	 * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  -   |
 	 * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
 	 * |  `   |A/SHFT|S/ALT |D/GUI |F/CTRL|   G  |-------.    ,-------|   H  |J/CTRL|K/GUI |L/ALT |;/SHFT|'/HYPR|
-	 * |------+------+------+------+------+------|BACKSP |    | ENT   |------+------+------+------+------+------|
+	 * |------+------+------+------+------+------| WINCT |    | ENT   |------+------+------+------+------+------|
 	 * |ADJUST|  Z   |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |  \   |
 	 * `-----------------------------------------/       /     \      \-----------------------------------------'
-	 *                   | MOUSE|      | L1   | /Space  /       \SHIFT \  |      |      | MEH  |
+	 *                   | MOUSE|      | L2   | /Space  /       \SHIFT \  |      |      | MEH  |
 	 *                   |      |      |      |/       /         \      \ | ENTR |      |      |
 	 *                   `----------------------------'           '------''--------------------'
 	 */
 	[_QWERTY] = LAYOUT(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC,
-			     KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS,
-			     KC_GRV, LSFT_T(KC_A), LALT_T(KC_S), LGUI_T(KC_D), LCTL_T(KC_F), KC_G, KC_H, RCTL_T(KC_J), RGUI_T(KC_K), RALT_T(KC_L), RSFT_T(KC_SCLN), HYPR_T(KC_QUOT),
-			     MO(_ADJUST), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_BSPC, KC_ENT, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_BSLS,
-                           MO(_MOUSE), XXXXXXX, MO(_L1), KC_SPC, KC_RSFT, KC_ENT, XXXXXXX, KC_MEH),
+			   KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS,
+			   KC_GRV, LSFT_T(KC_A), LALT_T(KC_S), LGUI_T(KC_D), LCTL_T(KC_F), KC_G, KC_H, RCTL_T(KC_J), RGUI_T(KC_K), RALT_T(KC_L), RSFT_T(KC_SCLN), HYPR_T(KC_QUOT),
+			   MO(_ADJUST), KC_Z, KC_X, KC_C, KC_V, KC_B, MO(_WINCT), KC_ENT, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_BSLS,
+			   MO(_MOUSE), XXXXXXX, MO(_L2), KC_SPC, KC_RSFT, KC_ENT, XXXXXXX, KC_MEH),
 /* L1
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
@@ -50,6 +51,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		       _______, TO(_QWERTY), _______, KC_LCBR, KC_RCBR, KC_F6, KC_F7, KC_F8, KC_EQL, KC_PLUS, _______, _______,
 		       _______, TO(_NAVIG), _______, KC_LPRN, KC_RPRN, MO(_WINCT), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, _______,
 		       _______, TO(_MOUSE), KC_PIPE, KC_LBRC, KC_RBRC, _______, _______, _______, _______, _______, KC_PGUP, KC_PGDN, _______, _______,
+		       _______, _______, _______, _______, _______, _______, _______, _______),
+/* L2
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |   !  |  @   |  #   |  $   |  %   |                    |  ^   |  &   |  *   |  (   |  )   |  |   |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |      |  =   |  [   |  ]   |  +   |-------.    ,-------| LEFT | DOWN |  UP  |RIGHT |  {   |  }   |
+ * |------+------+------+------+------+------| WINCT |    |       |------+------+------+------+------+------|
+ * |      |      |      |BUT_L |BUT_R |      |-------|    |-------|M_LEFT|M_DOWN| M_UP |M_RIGH|      |      |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   |      |      |      | /       /       \      \  |      |      |      |
+ *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   `----------------------------'           '------''--------------------'
+ */
+	[_L2] = LAYOUT(KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
+		       _______, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
+		       _______, _______, KC_EQL, KC_LBRC, KC_RBRC, KC_PLUS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_LCBR, KC_RCBR,
+		       _______, _______, _______, KC_BTN1, KC_BTN2, _______, MO(_WINCT), _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______,
 		       _______, _______, _______, _______, _______, _______, _______, _______),
 /* NAVIG
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -162,6 +182,9 @@ const char *read_layer_indicator(void)
 		break;
 	case _L1:
 		snprintf(layer_state_str, sizeof(layer_state_str), "L1");
+		break;
+	case _L2:
+		snprintf(layer_state_str, sizeof(layer_state_str), "L2");
 		break;
 	case _NAVIG:
 		snprintf(layer_state_str, sizeof(layer_state_str), "NAVIG");
