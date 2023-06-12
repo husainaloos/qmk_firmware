01d7ddf777f44334d574a19ea7952c52f6b36f34
Husain Alshehhi <husainaloos@users.nonreply.github.com>
2023-06-11 22:22:08 -0500

    lily58: match the dactyle mapping

--- keyboards/lily58/keymaps/husainaloos/keymap.c
+++ keyboards/lily58/keymaps/husainaloos/keymap.c
@@ -2,155 +2,54 @@
 #include <stdio.h>
 #include "lily58.h"
 
-enum layer_number {
-	_QWERTY = 0,
-	_L1,
-	_L2,
-	_NAVIG,
-	_WINCT,
-	_ADJUST,
-	_MOUSE,
-};
+#define _QWERTY 0
+#define _L1 1
+#define _L2 2
+#define _WIN 3
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	/* QWERTY_2
-	 * This is an experimental layer for now.
-	 * ,-----------------------------------------.                    ,-----------------------------------------.
-	 * |ESCAPE|   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |BACKSP|
-	 * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-	 * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  -   |
-	 * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-	 * |  `   |A/SHFT|S/ALT |D/GUI |F/CTRL|   G  |-------.    ,-------|   H  |J/CTRL|K/GUI |L/ALT |;/SHFT|'/HYPR|
-	 * |------+------+------+------+------+------| WINCT |    | ENT   |------+------+------+------+------+------|
-	 * |ADJUST|  Z   |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |  \   |
-	 * `-----------------------------------------/       /     \      \-----------------------------------------'
-	 *                   | MOUSE|      | L2   | /Space  /       \SHIFT \  |      |      | MEH  |
-	 *                   |      |      |      |/       /         \      \ | ENTR |      |      |
-	 *                   `----------------------------'           '------''--------------------'
-	 */
-	[_QWERTY] = LAYOUT(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC,
-			   KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS,
-			   KC_GRV, LSFT_T(KC_A), LALT_T(KC_S), LGUI_T(KC_D), LCTL_T(KC_F), KC_G, KC_H, RCTL_T(KC_J), RGUI_T(KC_K), RALT_T(KC_L), RSFT_T(KC_SCLN), HYPR_T(KC_QUOT),
-			   MO(_ADJUST), KC_Z, KC_X, KC_C, KC_V, KC_B, MO(_WINCT), KC_ENT, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_BSLS,
-			   MO(_MOUSE), XXXXXXX, MO(_L2), KC_SPC, KC_RSFT, KC_ENT, XXXXXXX, KC_MEH),
-/* L1
- * ,-----------------------------------------.                    ,-----------------------------------------.
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |QWERTY|      |  {   |  }   |  F6  |                    |  F7  |  F8  |  =   |  +   |      |      |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      | NAVIG|      |  (   |  )   |WINCT |-------.    ,-------| LEFT | DOWN |  UP  |RIGHT |      |      |
- * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |      | MOUSE|  |   |  [   |  ]   |      |-------|    |-------|      |      |PG_UP |PG_DN |      |      |
- * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |      |      |      | /       /       \      \  |      |      |      |
- *                   |      |      |      |/       /         \      \ |      |      |      |
- *                   `----------------------------'           '------''--------------------'
- */
-	[_L1] = LAYOUT(KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
-		       _______, TO(_QWERTY), _______, KC_LCBR, KC_RCBR, KC_F6, KC_F7, KC_F8, KC_EQL, KC_PLUS, _______, _______,
-		       _______, TO(_NAVIG), _______, KC_LPRN, KC_RPRN, MO(_WINCT), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, _______,
-		       _______, TO(_MOUSE), KC_PIPE, KC_LBRC, KC_RBRC, _______, _______, _______, _______, _______, KC_PGUP, KC_PGDN, _______, _______,
-		       _______, _______, _______, _______, _______, _______, _______, _______),
-/* L2
- * ,-----------------------------------------.                    ,-----------------------------------------.
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |   !  |  @   |  #   |  $   |  %   |                    |  ^   |  &   |  *   |  (   |  )   |  |   |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |  =   |  [   |  ]   |  +   |-------.    ,-------| LEFT | DOWN |  UP  |RIGHT |  {   |  }   |
- * |------+------+------+------+------+------| WINCT |    |       |------+------+------+------+------+------|
- * |      |      |      |BUT_L |BUT_R |      |-------|    |-------|M_LEFT|M_DOWN| M_UP |M_RIGH|      |      |
- * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |      |      |      | /       /       \      \  |      |      |      |
- *                   |      |      |      |/       /         \      \ |      |      |      |
- *                   `----------------------------'           '------''--------------------'
- */
-	[_L2] = LAYOUT(KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
-		       _______, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
-		       _______, _______, KC_EQL, KC_LBRC, KC_RBRC, KC_PLUS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_LCBR, KC_RCBR,
-		       _______, _______, _______, KC_BTN1, KC_BTN2, _______, MO(_WINCT), _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______,
-		       _______, _______, _______, _______, _______, _______, _______, _______),
-/* NAVIG
- * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |BUT_L |BUT_R |      |                    |      |      |      |      |      |      |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      | ACL0 | ACL1 | ACL2 |      |-------.    ,-------| M_L  | M_D  | M_UP | M_RT |      |      |
- * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |QWERTY|      |W_LEFT|W_UP  |W_DOWN|W_RIGH|-------|    |-------|      |      |      |      |      |      |
- * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |      |      |      | /       /       \      \  |      |      |      |
- *                   |      |      |      |/       /         \      \ |      |      |      |
- *                   `----------------------------'           '------''--------------------'
- */
-
-	[_NAVIG] = LAYOUT(XXXXXXX, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-			  XXXXXXX, XXXXXXX, XXXXXXX, KC_BTN1, KC_BTN2, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-			  XXXXXXX, XXXXXXX, KC_ACL0, KC_ACL1, KC_ACL2, XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, XXXXXXX, XXXXXXX,
-			  TO(_QWERTY), XXXXXXX, KC_WH_L, KC_WH_U, KC_WH_D, KC_WH_R, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-			  _______, _______, _______, _______, _______, _______, _______, _______),
-/* MOUSE
- * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |GUI+Z |GUI+X |GUI+C |GUI+V |      |-------.    ,-------|      |      |      |      |      |      |
- * |------+------+------+------+------+------| ENTER |    |       |------+------+------+------+------+------|
- * |QWERTY| UNDO | CUT  | COPY | PASTE|      |-------|    |-------|      |      |      |      |      |      |
- * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |      |      |      | /       /       \      \  |      |      |      |
- *                   |      |      |      |/       /         \      \ |      |      |      |
- *                   `----------------------------'           '------''--------------------'
- */
-
-	[_MOUSE] = LAYOUT(_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-			  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-			  _______, LGUI(KC_Z), LGUI(KC_X), LGUI(KC_C), LGUI(KC_V), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-			  TO(_QWERTY), KC_UNDO, KC_CUT, KC_COPY, KC_PASTE, XXXXXXX, KC_ENT, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-			  _______, _______, _______, _______, _______, _______, _______, _______),
-/* WINCT
- * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |      |                    |      |CENTER|FULL  |FULL-V|      |      |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------.    ,-------|      |H-THIR|H-TWOT|H-HALF|      |      |
- * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |QWERTY|      |      |      |      |      |-------|    |-------|      |VHALFU|VHALFD|      |      |      |
- * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |      |      |      | /       /       \      \  |      |      |      |
- *                   |      |      |      |/       /         \      \ |      |      |      |
- *                   `----------------------------'           '------''--------------------'
- */
-
-	[_WINCT] = LAYOUT(_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-			  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, LCA(KC_C), LCA(KC_ENT), MEH(KC_UP), XXXXXXX, XXXXXXX,
-			  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, LCA(KC_D), LCA(KC_T), LCA(KC_LEFT), XXXXXXX, XXXXXXX,
-			  TO(_QWERTY), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX, LCA(KC_UP), LCA(KC_DOWN), XXXXXXX, XXXXXXX, XXXXXXX,
-			  _______, _______, _______, _______, _______, _______, _______, _______),
-/* ADJUST
- * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |DM_REC|DM_STP|DM_PLY|      |      |                    |      |      |      |      |      |RESET |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      | SLEEP| WAKE |      |      |      |                    |      |      |      |      |      |      |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------.    ,-------|MUTE  |V_DOWN|V_UP  |      |      |      |
- * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|    |-------|      |      |      |      |      |POWER |
- * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |      |      |      | /       /       \      \  |      |      |      |
- *                   |      |      |      |/       /         \      \ |      |      |      |
- *                   `----------------------------'           '------''--------------------'
- */
-	[_ADJUST] = LAYOUT(XXXXXXX, DM_REC1, DM_RSTP, DM_PLY1, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RESET,
-			   XXXXXXX, KC_SLEP, KC_WAKE, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-			   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_KB_MUTE, KC_KB_VOLUME_DOWN, KC_KB_VOLUME_UP, XXXXXXX, XXXXXXX, XXXXXXX,
-			   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_SYSTEM_POWER,
-			   _______, _______, _______, _______, _______, _______, _______, XXXXXXX)
+  	[_QWERTY] = LAYOUT(
+		KC_ESC,		KC_1,		KC_2,		KC_3,		KC_4,		KC_5,		KC_6,		KC_7,		KC_8,		KC_9,		KC_0,		KC_BSPC,
+		KC_TAB,		KC_Q,		LT(_WIN,KC_W),	KC_E,		KC_R,		KC_T,		KC_Y,		ALL_T(KC_U),	KC_I,		KC_O,		KC_P,		KC_MINS,
+		KC_GRV,		SFT_T(KC_A),	ALT_T(KC_S),	GUI_T(KC_D),	CTL_T(KC_F),	KC_G,		KC_H,		CTL_T(KC_J),	GUI_T(KC_K),	ALT_T(KC_L),	SFT_T(KC_SCLN),	KC_QUOT,
+		KC_ENT,		KC_Z,		KC_X,		KC_C,		KC_V,		KC_B,		_______,	_______,	KC_N,		KC_M,		KC_COMM,	KC_DOT,		KC_SLSH,	KC_BSLASH,
+		_______,	_______,	MO(_L1),	KC_SPC,		LSFT_T(KC_ENT),	MO(_L2),	_______,	_______
+		),
+	[_L1]	  = LAYOUT(
+		KC_F12,		KC_F1,		KC_F2,		KC_F3,		KC_F4,		KC_F5,		KC_F6,		KC_F7,		KC_F8,		KC_F9,		KC_F10,		KC_F11,
+		_______,	_______,	_______,	KC_MS_UP,	_______,	_______,	KC_WH_L,	KC_WH_D,	KC_WH_U,	KC_WH_R,	_______,	_______,
+		_______,	G(KC_A),	KC_MS_LEFT,	KC_MS_DOWN,	KC_MS_RIGHT,	_______,	KC_LEFT,	KC_DOWN,	KC_UP,		KC_RIGHT,	_______,	_______,
+		QK_BOOT,	G(KC_Z),	G(KC_X),	G(KC_C),	G(KC_V),	KC_VOLD,	_______,	_______,	KC_VOLU,	KC_BTN1,	KC_BTN2,	_______,	_______,	_______,
+		_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______
+		),
+	[_L2]	  = LAYOUT(
+		_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,
+		_______,	_______,	_______,	_______,	KC_LCBR,	_______,	_______,	KC_RCBR,	_______,	_______,	_______,	_______,
+		_______,	_______,	_______,	KC_EQL,		KC_LPRN,	_______,	_______,	KC_RPRN,	KC_PLUS,	_______,	_______,	_______,
+		_______,	_______,	_______,	_______,	KC_LBRC,	_______,	_______,	_______,	_______,	KC_RBRC,	_______,	_______,	_______,	_______,
+		_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______
+		),
+	[_WIN]	  = LAYOUT(
+		_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,
+		_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	LCA(KC_D),	_______,	_______,	_______,
+		_______,	_______,	_______,	_______,	_______,	_______,	_______,	LCA(KC_LEFT),	LCA(KC_C),	LCA(KC_RIGHT),	_______,	_______,
+		_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	LCA(KC_ENT),	_______,	_______,	_______,
+		_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______
+		)
 };
+
+bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
+	switch (keycode) {
+        case SFT_T(KC_ENT):
+		// Force the dual-role key press to be handled as a modifier if any
+		// other key was pressed while the mod-tap key is held down.
+		return true;
+        default:
+		// Do not force the mod-tap key press to be handled as a modifier
+		// if any other key was pressed while the mod-tap key is held down.
+		return false;
+	}
+}
 
 layer_state_t layer_state_set_user(layer_state_t state)
 {
@@ -186,17 +85,8 @@ const char *read_layer_indicator(void)
 	case _L2:
 		snprintf(layer_state_str, sizeof(layer_state_str), "L2");
 		break;
-	case _NAVIG:
-		snprintf(layer_state_str, sizeof(layer_state_str), "NAVIG");
-		break;
-	case _MOUSE:
-		snprintf(layer_state_str, sizeof(layer_state_str), "MOUSE");
-		break;
-	case _WINCT:
-		snprintf(layer_state_str, sizeof(layer_state_str), "WINCT");
-		break;
-	case _ADJUST:
-		snprintf(layer_state_str, sizeof(layer_state_str), "ADJUST");
+	case _WIN:
+		snprintf(layer_state_str, sizeof(layer_state_str), "WIN");
 		break;
 	default:
 		snprintf(layer_state_str, sizeof(layer_state_str), "UNKNWN");
@@ -224,3 +114,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t * record)
 	}
 	return true;
 }
+
+/* Local Variables: */
+/* eval: (c-set-style "linux") */
+/* eval: (indent-tabs-mode 1) */
+/* compile-command: "qmk flash --keyboard lily58/rev1 --keymap husainaloos" */
+/* End: */
